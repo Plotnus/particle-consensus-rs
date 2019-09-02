9a66805c0aff8f48c4524e71aafc2964f59005d8
plot <plotnus@gmail.com>
2019-09-02 00:36:48 -0400

    add genome from cmdline string

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
+use clap::{App, Arg};
 use rand::distributions::{Distribution, Standard};
 use rand::Rng;
 use std::fmt;
+use std::io::{self, Write};
 use std::{thread, time};
 
 #[derive(Debug, PartialEq, Eq, Hash)]
@@ -15,10 +17,9 @@ struct Cell {
 impl Distribution<Cell> for Standard {
     fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> Cell {
         Cell {
-            state: if rng.gen_bool(0.5) {
-                State::On
-            } else {
-                State::Off
+            state: match rng.gen_bool(0.5) {
+                true => State::On,
+                false => State::Off,
             },
         }
     }
@@ -35,20 +36,50 @@ impl fmt::Display for Cell {
 }
 
 const NUM_CELLS_PER_CONFIGURATION: usize = 7;
-const CONFIGURATION_COUNT: i32 = 128; // 2 ^ NUM_CELLS_PER_CONFIGURATION
 
 struct Genome {
     sequence: Vec<bool>,
 }
 impl Genome {
+    const SIZE: usize = 128;
+
     fn new() -> Genome {
         let mut rng = rand::thread_rng();
         Genome {
-            sequence: (0..CONFIGURATION_COUNT)
-                .map(|_| rng.gen_bool(0.5))
-                .collect(),
+            sequence: (0..Genome::SIZE).map(|_| rng.gen_bool(0.5)).collect(),
         }
     }
+
+    fn from_string(s: &str) -> Genome {
+        assert_eq!(s.len(), Genome::SIZE / 4);
+
+        let mut sequence: Vec<bool> = Vec::with_capacity(Genome::SIZE);
+        for c in s.chars() {
+            match c {
+                '0' => sequence.append(&mut vec![false, false, false, false]),
+                '1' => sequence.append(&mut vec![true, false, false, false]),
+                '2' => sequence.append(&mut vec![false, true, false, false]),
+                '3' => sequence.append(&mut vec![true, true, false, false]),
+                '4' => sequence.append(&mut vec![false, false, true, false]),
+                '5' => sequence.append(&mut vec![true, false, true, false]),
+                '6' => sequence.append(&mut vec![false, true, true, false]),
+                '7' => sequence.append(&mut vec![true, true, true, false]),
+                '8' => sequence.append(&mut vec![false, false, false, true]),
+                '9' => sequence.append(&mut vec![true, false, false, true]),
+                'A' => sequence.append(&mut vec![false, true, false, true]),
+                'B' => sequence.append(&mut vec![true, true, false, true]),
+                'C' => sequence.append(&mut vec![false, false, true, true]),
+                'D' => sequence.append(&mut vec![true, false, true, true]),
+                'E' => sequence.append(&mut vec![false, true, true, true]),
+                'F' => sequence.append(&mut vec![true, true, true, true]),
+                _ => panic!("illegal character in genome {}", c),
+            }
+        }
+
+        Genome { sequence }
+    }
+
+    // TODO: clean this up... too complicated
     fn foo(&self, x: &[bool; NUM_CELLS_PER_CONFIGURATION]) -> State {
         let mut index = 0;
         if x[0] {
@@ -84,19 +115,81 @@ impl Genome {
 impl fmt::Display for Genome {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         // TODO convert this string to a cool HEX
-        for b in self.sequence.iter() {
-            write!(f, "{}", if *b { '1' } else { '0' })?
+        for chunk in self.sequence.chunks_exact(4) {
+            let c = match chunk {
+                &[false, false, false, false] => '0',
+                &[true, false, false, false] => '1',
+                &[false, true, false, false] => '2',
+                &[true, true, false, false] => '3',
+                &[false, false, true, false] => '4',
+                &[true, false, true, false] => '5',
+                &[false, true, true, false] => '6',
+                &[true, true, true, false] => '7',
+                &[false, false, false, true] => '8',
+                &[true, false, false, true] => '9',
+                &[false, true, false, true] => 'A',
+                &[true, true, false, true] => 'B',
+                &[false, false, true, true] => 'C',
+                &[true, false, true, true] => 'D',
+                &[false, true, true, true] => 'E',
+                &[true, true, true, true] => 'F',
+                _ => panic!("Genome sequence is not a multiple of chunk size"),
+            };
+            write!(f, "{}", c)?
         }
         Ok(())
     }
 }
 
 fn main() {
+    let matches = App::new("My Cellular Automaton")
+        .author("Jonathan P. <plotnus@gmail.com>")
+        .version("0.0.1")
+        .about("A classification cellular automaton.")
+        .arg(
+            Arg::with_name("genome")
+                .required(false)
+                .short("g")
+                .long("genome")
+                .takes_value(true),
+        )
+        .get_matches();
+
+    let genome = match matches.value_of("genome") {
+        Some(genome_string) => {
+            print!("Cloning genome");
+            io::stdout().flush();
+            for _ in 0..5 {
+                thread::sleep(time::Duration::from_millis(500));
+                print!(".");
+                io::stdout().flush();
+            }
+            thread::sleep(time::Duration::from_millis(250));
+            println!();
+            Genome::from_string(genome_string)
+        }
+        None => {
+            print!("No genome given.");
+
+            print!("Generating genome");
+            io::stdout().flush();
+
+            for _ in 0..5 {
+                thread::sleep(time::Duration::from_millis(500));
+                print!(".");
+                io::stdout().flush();
+            }
+
+            thread::sleep(time::Duration::from_millis(250));
+            println!("\nF6AAC27D1506EDBDF14D611C08DAC7EF6");
+            Genome::new()
+        }
+    };
+    //process::exit(1);
+
     const SLEEP_DURATION: time::Duration = time::Duration::from_millis(250);
     const TIMESTEP_COUNT: i32 = 40;
     const CELL_COUNT: usize = 48;
-
-    let genome: Genome = Genome::new();
 
     let mut cells: Vec<Cell> = (0..CELL_COUNT).map(|_| rand::random::<Cell>()).collect();
 
@@ -134,7 +227,8 @@ fn main() {
         thread::sleep(SLEEP_DURATION);
 
         cells.iter().for_each(|c| print!("{}", c));
-        print!("\n");
+        println!();
     }
-    print!("Genome:{}", genome);
+    println!();
+    println!("Genome:{}", genome);
 }
